6d5929dfe19ec1ddd14eb65bf8c13b75493018a9
Daniel Agyei <dandesign96@gmail.com>
2024-11-15 21:24:59 +0000

    added scrollreavel

---
+++ assets/js/animation.js
@@ -0,0 +1,51 @@
+const slideUp1 = document.querySelectorAll(".js-slide-up1");
+const slideUp2 = document.querySelectorAll(".js-slide-up2");
+const slideUp3 = document.querySelectorAll(".js-slide-up3");
+const fadeRight = document.querySelector(".js-fade-right");
+const slideRight = document.querySelector(".js-slide-right");
+const slideLeft = document.querySelector(".js-slide-left");
+const heroBanner = document.querySelector(".js-hero-banner");
+
+// Hero Section
+ScrollReveal().reveal(slideUp1, {
+  distance: "50%",
+  easing: "ease",
+  delay: 400,
+  duration: 650,
+});
+
+ScrollReveal().reveal(slideUp2, {
+  distance: "50%",
+  easing: "ease",
+  delay: 500,
+  duration: 650,
+});
+
+ScrollReveal().reveal(slideUp3, {
+  distance: "50%",
+  easing: "ease",
+  delay: 600,
+  duration: 650,
+});
+
+ScrollReveal().reveal(heroBanner, {
+  delay: 400,
+  duration: 550,
+  origin: "right",
+  mobile: false,
+});
+
+ScrollReveal().reveal(slideRight, {
+  distance: "30%",
+  easing: "ease",
+  delay: 500,
+  duration: 800,
+  origin: "right",
+});
+ScrollReveal().reveal(slideLeft, {
+  distance: "30%",
+  easing: "ease",
+  delay: 500,
+  duration: 800,
+  origin: "left",
+});
